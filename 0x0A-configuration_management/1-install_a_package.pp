066e579ddd8d5b91228497653c24f54f5ff87c85
ogetatonny <tonnyogeta7598@gmail.com>
2023-11-24 01:50:27 -0800

    corrected code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,26 @@
-#install flask using puppet
+# Ensure the stdlib and apt modules are installed
+class { 'stdlib': }
+class { 'apt': }
 
-package{'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install pip3
+package { 'python3-pip':
+  ensure => installed,
 }
+
+# Install Flask using pip3 with the specified version
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
+  require => [
+    Package['python3-pip'],
+    Class['apt'],
+  ],
+}
+
+# You might also want to notify services or restart your application
+# service { 'your_flask_service':
+#   ensure    => running,
+#   subscribe => Exec['install_flask'],
+# }
+
