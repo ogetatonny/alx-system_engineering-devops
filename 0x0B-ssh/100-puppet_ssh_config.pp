7848a8a42721506eadd5da7dce8a2027f9f00777
ogetatonny <tonnyogeta7598@gmail.com>
2023-12-06 06:37:16 -0800

    SSH corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #practice using Puppet to make changes to our configuration file
-#SSH client configuration must be configured to use the private key ~/.ssh/school
-#SSH client configuration must be configured to refuse to authenticate using a password
 
-include stdlib
-first_line {'turning of pwd based authentication':
+file {'etc/ssh/ssh_config':
   ensure  => present,
-  path    => '~/.ssh/ssh_config',
-  line    => 'PasswordAuthentication yes',
-  replace => true
+  content => "
+  
+  #SSH client configuration
+  host*
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+  "
 }
